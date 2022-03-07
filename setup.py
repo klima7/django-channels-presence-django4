5474d115e83f4676cfc510150ef3ef98e4534b63
klima7 <ukasz.klimkiewicz@gmail.com>
2022-03-07 22:32:03 +0100

    Migration to Django 4

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ with open(os.path.join(os.path.dirname(__file__), "README.rst")) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name="django-channels-presence",
-    version="1.0.0",
+    name="django4-channels-presence",
+    version="2.0.0",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
-    description='Tracking socket presence in "rooms" using django-channels',
+    description='Tracking socket presence in "rooms" using django-channels. Migrated to Django 4.',
     long_description=README,
-    url="https://github.com/ml-learning/django-channels-presence.git",
-    author="Charlie DeTar",
-    author_email="cfd@media.mit.edu",
+    url="https://github.com/klima7/django4-channels-presence",
+    author="Charlie DeTar, forked by Łukasz Klimkiewicz",
+    author_email="cfd@media.mit.edu, forked by ukasz.klimkiewicz@gmail.com",
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Django",
